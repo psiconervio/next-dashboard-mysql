16efdd97854ae1cf2642f7bee8e0252ec4bc1259
psiconervio <psiconervio@gmail.com>
2024-07-02 00:15:48 -0300

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
+
+import { montserrat } from './ui/fonts';
+import './ui/global.css'
+// estilos cargados globalmente
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${montserrat.className} antialiased`}>
+        <header>
+          <div>
+          <h1>h1 de header</h1>
+          </div>
+        </header>
+        {/* el children es el page.psx */}
+        {children}</body>
     </html>
   );
 }
