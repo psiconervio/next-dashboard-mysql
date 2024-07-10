5ebf873dedc9ab1e6c20cd5e3bb55592e726631a
psiconervio <psiconervio@gmail.com>
2024-07-10 05:48:16 -0300

    console ninja

--- app/page.tsx
+++ app/page.tsx
@@ -26,22 +26,20 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* <img src='/hero-desktop.png' alt='screenshot of te dashboard'></img> */}
-          {/* <Image
+          <Image
           src='/hero-desktop.png'
           alt='descripcion'
           width={1000}
           height={760}
-          className='hidden md:block'/> */}
+          className='hidden md:block'/>
         </div>
-        <Weather />
-        <UserList />
+
       </div>
     </main>
   );
